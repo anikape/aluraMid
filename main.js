07964a27631373863f70c6734c80146fc3b0f969
Ana Santos <ana.br@hotmail.com>
2023-04-23 17:43:33 -0300

    Projeto curso Desenvolve - Boticário.

---
+++ main.js
@@ -0,0 +1,12 @@
+function playSound() {
+  document.querySelector('.tecla_pom');
+  document.querySelector('#som_tecla_pom').play();
+
+  
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.map((lista) => {
+  
+})
